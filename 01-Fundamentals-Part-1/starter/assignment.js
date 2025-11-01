584f43b5279cb44ecee5221a8093b00b57f6a9fc
sn-vuoch <vouchra140305@gmail.com>
2025-11-01 10:42:57 +0700

    Finished Type conversion and coercion assignment

--- 01-Fundamentals-Part-1/starter/assignment.js
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -38,3 +38,10 @@ if (population > populationAverage) {
 } else {
   console.log("Portugal's population is 22 million below average");
 }
+
+// Lecture: Type conversion and coercion
+console.log("9" - "5"); // 4
+console.log("19" - "13" + "17"); // 617
+console.log("19" - "13" + 17); // 23
+console.log("123" < 57); // false
+console.log(5 + 6 + "4" + 9 - 4 - 2); // 1143
